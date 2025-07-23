a9af8cc8031ec842448ff4aef7d68b7e2c65c672
cucinellclark <clark@clswl097.cels.anl.gov>
2025-07-23 11:00:31 -0500

    fixing

--- bvbrcApi.js
+++ bvbrcApi.js
@@ -1,4 +1,4 @@
-define(['./bvbrcApiCore.js'], function (mod) {
+define(['./bvbrcApiCore'], function (mod) {
   // Support both default export (ESM) and named exports
   return mod.default || mod;
 });
